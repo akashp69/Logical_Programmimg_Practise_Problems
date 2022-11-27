f8813499fc2f18d3fccfeb13cd9a116c60c62fc7
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:51:55 +0530

    Update PerfectNumber.java

--- src/com/bridgelabz/logicalprograms/PerfectNumber.java
+++ src/com/bridgelabz/logicalprograms/PerfectNumber.java
@@ -4,6 +4,9 @@ package com.bridgelabz.logicalprograms;
 import java.util.Scanner;
 
 public class PerfectNumber {
+     /**
+    *This  Method is Created For get Perfect Number
+    */
     public static void main(String[] args) {
 
         System.out.println("Enter the Number to check whether it is perfect number or not; Number");
@@ -11,6 +14,9 @@ public class PerfectNumber {
         int Number =scanner. nextInt();
 
         int sum=0;
+    /**
+    *Using For loop for gets values to find the Perfect Number
+    */
         for(int i =1;i<Number;i++) {
             if(Number % i ==0) {
                 sum = i;
@@ -18,6 +24,9 @@ public class PerfectNumber {
                 sum = sum +i ;
             }
         }
+    /**
+    *Using if-else loop to find the Perfect Number
+    */
         if (sum==Number) {
             System.out.print(+Number  + " " + "is perfect number");
         }
